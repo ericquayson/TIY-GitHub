645eaa6785ad1ea4ade86134083d3702e4ac5244
ericquayson <ericquayson@github.com>
2015-10-20 21:15:41 -0400

    baby steps, grabbed avatar, name, login, company, location, email, blog and created at with a date filter

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,20 @@ $(document).ready(function) {
 
 ;(function () {
   angular.module('TIY-Github', [])
-
+// Grabbing profile data from the ericquayson.json
+  .run(function($http, $rootScope) {
+    $http.get('/apis/github/users/ericquayson.json')
+    .then(function(response) {
+    // Baby steps young grasshopper
+      $rootScope.avatar_url = response.data.avatar_url;
+      $rootScope.name = response.data.name;
+      $rootScope.login = response.data.login;
+      $rootScope.company = response.data.company;
+      $rootScope.location = response.data.location;
+      $rootScope.email = response.data.email;
+      $rootScope.blog = response.data.blog;
+      $rootScope.created_at = response.data.created_at;
+    })
+  });
 
 })();
